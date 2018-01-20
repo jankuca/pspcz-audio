d42dad5a690ea7c64831860e7d51fce91ee4f8f4
jankuca <jan@jankuca.com>
2018-01-20 23:03:41 +0100

    add <meta> og:image tag

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Head from 'next/head'
+
+
 export default ({ children }) => (
   <div style={{
     fontFamily: '"Baskerville", serif',
@@ -9,6 +12,10 @@ export default ({ children }) => (
     margin: '20px auto 50px',
     padding: '20px',
   }}>
+    <Head>
+      <meta property="og:image" content="http://pspcz-audio.herokuapp.com/static/logo-poslanecka-snemovna-parlamentu-cr.png" />
+    </Head>
+
     <img
       src="/static/logo-poslanecka-snemovna-parlamentu-cr.png"
       alt="Poslanecká sněmovna Parlamentu České republiky"
